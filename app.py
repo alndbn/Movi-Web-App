bc6cb08f69caabd4512cc13b4e7c46fbf9190fee
Alena Drobner <alena.drobner@gmail.com>
2025-12-09 14:55:10 +0100

    Directory for HTML templates, set URL routes.

--- app.py
+++ app.py
@@ -16,6 +16,37 @@ data_manager = DataManager()
 def home():
     return "Welcome to MoviWeb App!"
 
+@app.route('/users')
+def list_users():
+    users = data_manager.get_users()
+    return str(users)
+
+
+@app.route('/users', methods=['POST'])
+def add_new_user(name):
+    data_manager.create_user(name)
+    return f"New user {name} created."
+
+@app.route('/users/<int:user_id>/movies', methods=['GET'])
+def get_list_of_fav_movies(user_id):
+    return str(data_manager.get_movies(user_id))
+
+@app.route('/users/<int:user_id>/movies', methods=['POST'])
+def add_new_fav_movie(movie, user_id):
+    movie["user_id"] = user_id
+    data_manager.add_movie(movie)
+    return f"For this user ID I created this {movie}"
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
+def change_movie_title(title, user_id, movie_id):
+    data_manager.update_movie(movie_id, title)
+    return "Successfully updated."
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
+def delete_fav_movie(user_id, movie_id):
+    data_manager.delete_movie(user_id)
+    return "Successfully deleted."
 
 if __name__ == '__main__':
     with app.app_context():
